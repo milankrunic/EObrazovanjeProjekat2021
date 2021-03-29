52acdd29ac3a009b11632ab544275c45e545ce09
milankrunic <milankrunic95@gmail.com>
2021-03-29 18:34:56 +0200

    Dodate TeachingTypeDTO i TeachingDTO klase

---
+++ EObrazivanjeProjekat/src/main/java/ftn/eObrazovanjeProjekat/EObrazivanjeProjekat/dto/TeachingDTO.java
@@ -0,0 +1,80 @@
+package ftn.eObrazovanjeProjekat.EObrazivanjeProjekat.dto;
+
+import ftn.eObrazovanjeProjekat.EObrazivanjeProjekat.model.Teaching;
+
+public class TeachingDTO {
+	
+	private Long idTeaching;
+	private Long courseInstance;
+	private Long teacher;
+	private Long teachingType;
+	private String nameTeacher;
+	private String nameTeachingType;
+	
+	public TeachingDTO() {
+		super();
+	}
+
+	public TeachingDTO(Long idTeaching, Long courseInstance, Long teacher, Long teachingType, String nameTeacher,
+			String nameTeachingType) {
+		super();
+		this.idTeaching = idTeaching;
+		this.courseInstance = courseInstance;
+		this.teacher = teacher;
+		this.teachingType = teachingType;
+		this.nameTeacher = nameTeacher;
+		this.nameTeachingType = nameTeachingType;
+	}
+	
+	public TeachingDTO(Teaching te) {
+		this(te.getIdTeaching(), te.getCourseInstance().getIdCourseInstance(), te.getTeacher().getIdTeacher(), te.getTeachingType().getIdTeachingType(), te.getTeacher().getLastName(), te.getTeachingType().getName());
+	}
+
+	public Long getIdTeaching() {
+		return idTeaching;
+	}
+
+	public void setIdTeaching(Long idTeaching) {
+		this.idTeaching = idTeaching;
+	}
+
+	public Long getCourseInstance() {
+		return courseInstance;
+	}
+
+	public void setCourseInstance(Long courseInstance) {
+		this.courseInstance = courseInstance;
+	}
+
+	public Long getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(Long teacher) {
+		this.teacher = teacher;
+	}
+
+	public Long getTeachingType() {
+		return teachingType;
+	}
+
+	public void setTeachingType(Long teachingType) {
+		this.teachingType = teachingType;
+	}
+
+	public String getNameTeacher() {
+		return nameTeacher;
+	}
+
+	public void setNameTeacher(String nameTeacher) {
+		this.nameTeacher = nameTeacher;
+	}
+
+	public String getNameTeachingType() {
+		return nameTeachingType;
+	}
+
+	public void setNameTeachingType(String nameTeachingType) {
+		this.nameTeachingType = nameTeachingType;
+	}
+}
