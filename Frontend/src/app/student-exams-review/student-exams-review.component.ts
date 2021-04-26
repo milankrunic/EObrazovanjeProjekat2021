9f431bf24613c10ead4c77109a79e895cf352b70
Dario <zigadario@gmail.com>
2021-04-26 11:20:18 +0200

    Implemented student exams review and improved minor tweaks.

--- Frontend/src/app/student-exams-review/student-exams-review.component.ts
+++ Frontend/src/app/student-exams-review/student-exams-review.component.ts
@@ -12,4 +12,33 @@ export class StudentExamsReviewComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  title = 'Student exams review table';
+
+  headers = ["Row number", "Subject ID", "Subject name", "ECTS", "Teacher", "Grade"];
+
+  rows = [{
+    "Row number" : "1",
+    "Subject ID": "SIT04",
+    "Subject name" : "Mathematics",
+    "ECTS" : "8",
+    "Teacher" : "Milos Milakovic",
+    "Grade" : "8"
+  },
+  {
+    "Row number" : "2",
+    "Subject ID": "SIT01",
+    "Subject name" : "Object Oriented Programming",
+    "ECTS" : "8",
+    "Teacher" : "Milorad Miloradovic",
+    "Grade" : "7"
+  },
+  {
+    "Row number" : "3",
+    "Subject ID": "SIT02",
+    "Subject name" : "English",
+    "ECTS" : "4",
+    "Teacher" : "Ana Bursac",
+    "Grade" : "10"
+  }]
+  
 }
