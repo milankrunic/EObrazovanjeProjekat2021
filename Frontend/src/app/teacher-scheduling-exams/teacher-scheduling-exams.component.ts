dc8f179af69b533fc01aeb35fc6e7e946fe3590f
Dario <zigadario@gmail.com>
2021-04-27 11:30:05 +0200

    Added teacher sheduling exams web page.

--- Frontend/src/app/teacher-scheduling-exams/teacher-scheduling-exams.component.ts
+++ Frontend/src/app/teacher-scheduling-exams/teacher-scheduling-exams.component.ts
@@ -12,4 +12,25 @@ export class TeacherSchedulingExamsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  title = 'Teacher schedule exams table';
+
+  headers = ["Name of exam", "Date and Time", "Teacher"];
+
+  rows = [{
+    "Name of exam" : "Osnove programiranja",
+    "Date and Time": "10.04.2021 17:30",
+    "Teacher" : "Petar Petrovic"
+  },
+  {
+    "Name of exam" : "Osnove softverskih arhitektura",
+    "Date and Time": "08.04.2021 14:00",
+    "Teacher" : "Petar Petrovic"
+  },
+  {
+    "Name of exam" : "XML tehnologije",
+    "Date and Time": "05.04.2021 12:30",
+    "Teacher" : "Petar Petrovic"
+  }]
+
+
 }
