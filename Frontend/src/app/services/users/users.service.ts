144adab0ac1e8ab1e4fcfc3cb0fc7bb7586a3e1b
maricm123 <mihailomaric001@gmail.com>
2021-05-01 09:33:38 +0200

    user service

--- Frontend/src/app/services/users/users.service.ts
+++ Frontend/src/app/services/users/users.service.ts
@@ -19,4 +19,19 @@ export class UsersService {
     return this.http.get<user[]>(this.usersUrl);
   }
 
+  get(id:any):Observable<user>{
+    // const params: HttpParams = new HttpParams().set('_id',id);
+    return this.http.get<user>(this.usersUrl+`/${id}`);
+  }
+
+  save(users:user){
+    return this.http.post(this.usersUrl,user);
+  }
+
+  delete(idUser:any){
+    return this.http.delete(this.usersUrl+`/${idUser}`);
+  }
+
+  
+
 }
