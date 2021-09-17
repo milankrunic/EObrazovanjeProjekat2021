959d75639029bc7e80559b5f66cd4c373009dcc9
maricm123 <mihailomaric001@gmail.com>
2021-09-17 11:41:15 +0200

    permit all api/**

--- EObrazivanjeProjekat/src/main/java/ftn/eObrazovanjeProjekat/EObrazivanjeProjekat/security/SecurityConfiguration.java
+++ EObrazivanjeProjekat/src/main/java/ftn/eObrazovanjeProjekat/EObrazivanjeProjekat/security/SecurityConfiguration.java
@@ -66,7 +66,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.and()
 			.authorizeRequests()
 				.antMatchers("/", "/api/login", "/styles.css", "/profile", 
-						"api/admin",
+						"/api/admin", "/api/**",
                         "/runtime.js", "/polyfills.js", "/vendor.js", "/main.js", "/images/**", "/js/**",
                         "/runtime-es2015.js", "/polyfills-es2015.js", "/vendor-es2015.js", "/main-es2015.js").permitAll()
 				.antMatchers("/index.html").authenticated()
