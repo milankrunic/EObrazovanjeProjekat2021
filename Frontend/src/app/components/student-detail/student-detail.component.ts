b4e02ace4b23c71cdd786b3b915f2d840c4f022c
ivans1999 <stankovicivan1999@gmail.com>
2022-01-26 17:01:21 +0100

    added file saver,working on documents view

--- Frontend/src/app/components/student-detail/student-detail.component.ts
+++ Frontend/src/app/components/student-detail/student-detail.component.ts
@@ -1,4 +1,15 @@
+import { Location } from '@angular/common';
+import { stringify } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { CourseInstance } from 'src/app/model/courseInstance';
+import { Payment } from 'src/app/model/payment';
+import { Role } from 'src/app/model/role';
+import { Student } from 'src/app/model/student';
+import { AuthenticationService } from 'src/app/services/authentication-service.service';
+import { StudentService } from '../student/student.service';
+import { UserService } from '../users/users.service';
 
 @Component({
   selector: 'app-student-detail',
@@ -7,9 +18,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentDetailComponent implements OnInit {
 
-  constructor() { }
+  student: Student;
+  courseI:boolean=false;
+  courses:CourseInstance[] = [];
+  documentI:boolean=false;
+  documents:Document[] = [];
+  paymentsI:boolean=false;
+  payments:Payment[] = [];
+  
+  // role:Role=new Role({
+  //   id:0,
+  //   code:'',
+  //   name:''
+  // })
 
-  ngOnInit(): void {
+  constructor(private location: Location,private studentS: StudentService,private userService: UserService, private route: ActivatedRoute,private router: Router) { 
+    // this.role = auths.getRole();
+    
+    this.student = new Student({
+      id:0,
+      cardNumber: '',
+      userDTO:{
+        id:0,
+        firstName:'',
+        lastName:'',
+        userName:'',
+        password:'',
+        roles:[]
+      }
+    })
   }
 
+  ngOnInit(): void {
+    if (this.route.snapshot.params['id']) {
+      this.route.params.pipe(switchMap((params: Params) => 
+          this.userService.getStudent(+params['id']))) // convert to number
+        .subscribe(res => {
+          this.student = res.body==null ? this.student:res.body;
+          this.studentS.setStudent(this.student);
+          this.courseI = true;
+          }
+        );
+    }
+  }
+
+  click(button:string){
+    this.documentI = false;
+    this.paymentsI = false;
+    this.courseI = false;
+    
+    if(button === 'documents'){
+      this.documentI = true;
+    }else if(button === 'payments'){
+      this.paymentsI = true;
+    }else if(button === 'courses'){
+      this.courseI = true;
+    }
+  }
+
+  goBack(): void {
+    console.log(this.location)
+    this.location.back();
+  }
 }
