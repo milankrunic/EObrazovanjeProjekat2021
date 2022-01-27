324566da677091e4e669990ea8b7150d3c01d777
ivans1999 <stankovicivan1999@gmail.com>
2022-01-27 17:19:51 +0100

    list of teachers for admin.

--- Frontend/src/app/components/teachers/teachers.component.ts
+++ Frontend/src/app/components/teachers/teachers.component.ts
@@ -1,4 +1,8 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Teacher } from 'src/app/model/teacher';
+import { UserService } from '../users/users.service';
 
 @Component({
   selector: 'app-teachers',
@@ -7,9 +11,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeachersComponent implements OnInit {
 
-  constructor() { }
+  teachers:Teacher[] | null = [];
+  numberPages:number[] = [];
+  numberPage:number = 0;
+
+  constructor(private location: Location, private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
+    this.getTeachers();
   }
 
+  getTeachers(){
+    this.getNumberPages();
+    this.userService.getTeachers(this.numberPage,'').subscribe(res=>{
+      this.teachers = res.body;
+    });
+  }
+
+  getNumberPages(){
+    this.userService.getNumberPage('TEACHERS').subscribe(res =>{
+      const num = res.body == null ? 0:res.body;
+      var i = 1;
+      this.numberPages = [];
+      for (let index = 0; index < num; index++) {
+        this.numberPages.push(i);
+        i++;
+      }
+    })
+  }
+
+  goToTeacher(teacher:Teacher){
+    this.router.navigate(['/teacher', teacher.id]);
+  }
+
+  deleteTeacher(t: Teacher): void{
+    this.userService.deleteTeacher(t.id==undefined ? 0:t.id).subscribe(
+      () => this.getTeachers()
+    )
+  }
+
+  increaseNumberPage(){
+    if(this.numberPage < this.numberPages.length-1){
+      this.numberPage=this.numberPage+1;
+      this.getTeachers();
+    }
+  }
+
+  reduceNumberPage(){
+    if(this.numberPage>=1){
+      this.numberPage=this.numberPage-1;
+      this.getTeachers();
+    }
+  }
+
+  setNumberPage(numberPage:number){
+    this.numberPage = numberPage-1;
+    this.getTeachers();
+  }
+
+  isActive(num:number):boolean{
+    if(this.numberPage===num){
+      return true;
+    }
+    return false;
+  }
+
+  goBack(): void {
+    console.log(this.location)
+    this.location.back();
+  }
 }
