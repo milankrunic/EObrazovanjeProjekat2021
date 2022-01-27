620eeb2c1819d8476d235ee814f24a34f1ccdc60
ivans1999 <stankovicivan1999@gmail.com>
2022-01-27 17:02:40 +0100

    courses view.

--- Frontend/src/app/components/courses/courses.component.ts
+++ Frontend/src/app/components/courses/courses.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { CourseInstance } from 'src/app/model/courseInstance';
+import { Teacher } from 'src/app/model/teacher';
+import { AuthenticationService } from 'src/app/services/authentication-service.service';
+import { StudentService } from '../student/student.service';
+import { TeacherComponent } from '../teacher/teacher.component';
+import { UserService } from '../users/users.service';
+import { CoursesService } from './courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -7,9 +17,117 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+  teacher:Teacher = new Teacher({
+    id:0,
+    userDTO:{
+      id:0,
+      firstName:'',
+      lastName:'',
+      userName:'',
+      password:'',
+      roles:[]
+    }
+  })
+
+  role = '';
+  numberPages:number[] = [];
+  numberPage:number = 0;
+  btnAdd:boolean = true;
+  mode = '';
+  coursesIntances: CourseInstance[] | null = [];
+
+  subscription: Subscription;
+
+  constructor(
+    private courseService: CoursesService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private authS:AuthenticationService,
+    private userService: UserService) { 
+    this.subscription = courseService.RegenerateData$.subscribe(() => 
+      this.getCoursesInstances(this.mode)
+    );
+    this.role = authS.getRole()
+  }
+
+  getNumberPages(mode:string){
+    this.courseService.getNumberPage(mode,this.teacher.userDTO.userName,-1).subscribe(res =>{
+      const num = res.body == null ? 0:res.body;
+      var i = 1;
+      this.numberPages = [];
+      for (let index = 0; index < num; index++) {
+        this.numberPages.push(i);
+        i++;
+      }
+    })
+  }
 
   ngOnInit(): void {
+    if (this.route.snapshot.params['id']) {
+      this.btnAdd = false;
+      this.route.params.pipe(switchMap((params: Params) => 
+      this.userService.getTeacher(+params['id']))) // convert to number
+    .subscribe(res => {
+      this.mode = 'TEACHER';
+      this.teacher = res.body==null ? this.teacher:res.body;
+      this.getCoursesInstances(this.mode);
+      }
+    );
+    }else{
+      this.mode = 'ADMIN';
+      this.getCoursesInstances(this.mode);
+    }
+  }
+
+  getCoursesInstances(mode:string){
+    this.getNumberPages(mode);
+    this.courseService.getCoursesInstances(this.teacher.userDTO.userName,this.numberPage).subscribe(
+      response => {
+        this.coursesIntances = response.body;
+      });
+  }
+
+  deleteCourseInstance(courseInstance: CourseInstance): void {
+    console.log("Brisem: "+JSON.stringify(courseInstance));
+    this.courseService.deleteCourseInstance(courseInstance.id==undefined ? 0:courseInstance.id).subscribe(
+      () => this.getCoursesInstances(this.mode)
+    );
+  }
+
+  goToCourseInstance(courseInstance: CourseInstance): void {
+    this.router.navigate(['/course-instance', courseInstance.id]);
+  }
+
+  dateToString(date:Date):Date{
+    var d = new Date(date);
+    d.setHours(d.getHours()-1);
+    return d;
+  }
+
+  increaseNumberPage(){
+    if(this.numberPage < this.numberPages.length-1){
+      this.numberPage=this.numberPage+1;
+      this.getCoursesInstances(this.mode);
+    }
+  }
+
+  reduceNumberPage(){
+    if(this.numberPage>=1){
+      this.numberPage=this.numberPage-1;
+      this.getCoursesInstances(this.mode);
+    }
+  }
+
+  setNumberPage(numberPage:number){
+    this.numberPage = numberPage-1;
+    this.getCoursesInstances(this.mode);
+  }
+
+  isActive(num:number):boolean{
+    if(this.numberPage===num){
+      return true;
+    }
+    return false;
   }
 
 }
--- Frontend/src/app/model/teacher.ts
+++ Frontend/src/app/model/teacher.ts
@@ -1,9 +1,12 @@
 import { User } from "./user";
 
-export class Teacher {
-    id?:any;
-    firstName:string;
-    lastName:string;
-    email:string;
-    user:string;
-  }
+export class Teacher{
+    public id: number;
+    public userDTO: User;
+
+    constructor(teacherCfg:Teacher)
+    {
+        this.id = teacherCfg.id;
+        this.userDTO = teacherCfg.userDTO;
+    }
+}
