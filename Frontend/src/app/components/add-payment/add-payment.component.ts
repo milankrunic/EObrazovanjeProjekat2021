d797117d7e943120ed254b03e6db3f4cb9e56e8f
zigad <zigad@192.168.0.219>
2022-01-31 01:17:12 +0100

    Fixed sidebar error.

--- Frontend/src/app/components/add-payment/add-payment.component.ts
+++ Frontend/src/app/components/add-payment/add-payment.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-payment',
+  selector: 'app-add-payment',
   templateUrl: './add-payment.component.html',
   styleUrls: ['./add-payment.component.css']
 })
