fdf8f1c08cf006e507ee4d8fdafe641f54e978de
ivans1999 <stankovicivan1999@gmail.com>
2022-01-31 14:53:57 +0100

    pw changer done.

--- Frontend/src/app/components/users/users.service.ts
+++ Frontend/src/app/components/users/users.service.ts
@@ -119,7 +119,7 @@ export class UserService {
     }
 
     changePass(changePass: ChangePass): Observable<HttpResponse<User>>{
-        const url = `api/users/change-password`;
+        const url = `http://localhost:8080/api/users/change-password`;
         return this.http.post<User>(url, changePass, {observe: 'response'});
     }
     // getUserRoles(userId: number): Observable<HttpResponse<Role[]>> {
