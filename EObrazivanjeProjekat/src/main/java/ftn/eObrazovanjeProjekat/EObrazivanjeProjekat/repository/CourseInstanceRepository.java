645e14c418a3e74ff69903b57c581cfe6923daf7
maricm123 <mihailomaric001@gmail.com>
2022-02-05 12:30:08 +0100

    fixed repository

--- EObrazivanjeProjekat/src/main/java/ftn/eObrazovanjeProjekat/EObrazivanjeProjekat/repository/CourseInstanceRepository.java
+++ EObrazivanjeProjekat/src/main/java/ftn/eObrazovanjeProjekat/EObrazivanjeProjekat/repository/CourseInstanceRepository.java
@@ -15,30 +15,30 @@ public interface CourseInstanceRepository extends JpaRepository<CourseInstance, 
 	CourseInstance findByIdCourseInstance(Long id);
 	
 	@Query(value = "SELECT * FROM studentskasluzba.course_instance\r\n" + 
-			"	where id in (SELECT id_course_instance FROM studentskasluzba.teaching\r\n" + 
-			"					where id_teacher in (SELECT id FROM studentskasluzba.teacher\r\n" + 
-			"											where user_id in (SELECT id FROM studentskasluzba.user\r\n" + 
+			"	where id_course_instance in (SELECT course_instance FROM studentskasluzba.teaching\r\n" + 
+			"					where teacher in (SELECT id_teacher FROM studentskasluzba.teacher\r\n" + 
+			"											where id_user in (SELECT id_user FROM studentskasluzba.user\r\n" + 
 			"												where username LIKE :userName)))", nativeQuery = true)
 	Page<CourseInstance> findByTeacher(@Param("userName") String username,Pageable page);
 	
 	@Query(value = "SELECT count(*) FROM studentskasluzba.course_instance\r\n" + 
-			"				where id in (SELECT id_course_instance FROM studentskasluzba.teaching\r\n" + 
-			"								where id_teacher in (SELECT id FROM studentskasluzba.teacher\r\n" + 
-			"														where user_id in (SELECT id FROM studentskasluzba.user\r\n" + 
+			"				where id_course_instance in (SELECT course_instance FROM studentskasluzba.teaching\r\n" + 
+			"								where teacher in (SELECT id_teacher FROM studentskasluzba.teacher\r\n" + 
+			"														where id_user in (SELECT id_user FROM studentskasluzba.user\r\n" + 
 			"															where username LIKE :userName)))", nativeQuery = true)
 	Long countForTeacher(@Param("userName") String username);
 	
 	@Query(value = "SELECT * FROM studentskasluzba.course_instance\r\n" + 
-			"	where id in (SELECT id_course_instance FROM studentskasluzba.enrollment\r\n" + 
-			"					where id_student in (SELECT id FROM studentskasluzba.student\r\n" + 
-			"											where user_id in (SELECT id FROM studentskasluzba.user\r\n" + 
+			"	where id_course_instance in (SELECT course_instance FROM studentskasluzba.enrollment\r\n" + 
+			"					where student in (SELECT id_student FROM studentskasluzba.student\r\n" + 
+			"											where id_user in (SELECT id_user FROM studentskasluzba.user\r\n" + 
 			"												where username LIKE :userName)))", nativeQuery = true)
 	Page<CourseInstance> findByStudent(@Param("userName") String username,Pageable page);
 
 	@Query(value = "SELECT count(*) FROM studentskasluzba.course_instance\r\n" + 
-			"				where id in (SELECT course_instance_id FROM studentskasluzba.enrollment\r\n" + 
-			"								where id_student in (SELECT id FROM studentskasluzba.student\r\n" + 
-			"														where user_id in (SELECT id FROM studentskasluzba.user\r\n" + 
+			"				where id_course_instance in (SELECT course_instance FROM studentskasluzba.enrollment\r\n" + 
+			"								where student in (SELECT id_student FROM studentskasluzba.student\r\n" + 
+			"														where id_user in (SELECT id_user FROM studentskasluzba.user\r\n" + 
 			"															where username LIKE :userName)))", nativeQuery = true)
 	Long countForStudent(@Param("userName") String username);
 
